1e16a396d86f50fb76142f00391ae6bb3e33270f
JeongTJ <dmswl022329@gmail.com>
2025-06-30 13:54:01 +0900

    [FEAT] 토큰 발급 및 재발급 시 데이터베이스 저장

--- backend/srcs/services/authServices.js
+++ backend/srcs/services/authServices.js
@@ -33,44 +33,47 @@ const generateTokens = async (id) => {
 		process.env.JWT_SECRET, 
 		{ expiresIn: '1h',}
 	);
-
+	
+	await membersServices.update(member.id, { refresh_token: refreshToken });
+	
 	return { refreshToken, accessToken };
 }
 
 const refreshToken = async (refreshToken) => {
 	const decoded = jwt.verify(refreshToken, process.env.JWT_SECRET);
 	const member = await membersServices.findById(decoded.id);
-
+	
 	if (!member) {
 		const error = new Error(`Member with id ${decoded.id} not found.`);
 		error.statusCode = 404;
 		throw error;
 	}
-
+	
 	if (decoded.type !== 'refresh') {
 		const error = new Error('Invalid token type. An access token is required.');
 		error.statusCode = 401;
 		throw error;
 	}
-
+	
 	if (member.refresh_token !== refreshToken) {
 		const error = new Error('Invalid refresh token.');
 		error.statusCode = 401;
 		throw error;
 	}
-
+	
 	const newRefreshToken = jwt.sign(
 		{ id: parseInt(member.id, 10), type: 'refresh' }, 
 		process.env.JWT_SECRET,
 		{ expiresIn: '1d' }
 	);
-
+	
 	const newAccessToken = jwt.sign(
 		{ id: parseInt(member.id, 10), type: 'access' }, 
 		process.env.JWT_SECRET,
 		{ expiresIn: '1d' }
 	);
-
+	await membersServices.update(member.id, { refresh_token: newRefreshToken });
+	
 	return { newRefreshToken, newAccessToken };
 }
 
