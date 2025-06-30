d6769ec41c4da8128cf64c8772c2a80003c4f6a8
JeongTJ <dmswl022329@gmail.com>
2025-06-30 14:16:42 +0900

    [FEAT] 뷰에서 리프레시 토큰 삭제

--- backend/srcs/repository/membersRepository.js
+++ backend/srcs/repository/membersRepository.js
@@ -3,18 +3,26 @@ const { pool } = require('../config/psqlConfig');
 // 모든 회원 찾기
 const findAll = async () => {
 	const { rows } = await pool.query('SELECT * FROM members ORDER BY id ASC');
+	for (const row of rows) {
+		delete row.password;
+		delete row.refresh_token;
+	}
 	return rows;
 };
 
 // 특정 회원 id로 찾기
 const findByUserId = async (user_id) => {
 	const { rows } = await pool.query('SELECT * FROM members WHERE user_id = $1', [user_id]);
+	delete rows[0].password;
+	delete rows[0].refresh_token;
 	return rows[0];
 };
 
 // 특정 회원 db id로 찾기
 const findById = async (id) => {
 	const { rows } = await pool.query('SELECT * FROM members WHERE id = $1', [id]);
+	delete rows[0].password;
+	delete rows[0].refresh_token;
 	return rows[0];
 };
 
@@ -27,12 +35,15 @@ const create = async (memberData) => {
 		[user_id, name, email, password]
 	);
 	delete rows[0].password;
+	delete rows[0].refresh_token;
 	return rows[0];
 };
 
 // 특정 회원 이메일로 찾기
 const findByEmail = async (email) => {
 	const { rows } = await pool.query('SELECT * FROM members WHERE email = $1', [email]);
+	delete rows[0].password;
+	delete rows[0].refresh_token;
 	return rows[0];
 };
 
@@ -42,16 +53,17 @@ const update = async (id, memberData) => {
 	const { rows } = await pool.query(
 		`UPDATE members 
 		SET 
-			name = COALESCE($1, name),
-			profile = COALESCE($2, profile), 
-			status_note = COALESCE($3, status_note), 
-			refresh_token = COALESCE($4, refresh_token), 
-			updated_at = now() 
+		name = COALESCE($1, name),
+		profile = COALESCE($2, profile), 
+		status_note = COALESCE($3, status_note), 
+		refresh_token = COALESCE($4, refresh_token), 
+		updated_at = now() 
 		WHERE id = $5 
 		RETURNING *`,
 		[name, profile, status_note, refresh_token, id]
 	);
 	delete rows[0].password;
+	delete rows[0].refresh_token;
 	return rows[0];
 };
 
