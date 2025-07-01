a97c0b9bd3e1899a66c4c88108f21faa59de36aa
JeongTJ <dmswl022329@gmail.com>
2025-07-01 16:51:23 +0900

    [FEAT] 개발, 배포버전 분리

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,24 +1,16 @@
 #!/bin/sh
 set -e
 
-# # 데이터베이스가 준비될 때까지 대기 (최대 30초)
-# MAX_ATTEMPTS=30
-# until npx sequelize-cli db:version >/dev/null 2>&1; do
-#   if [ $MAX_ATTEMPTS -le 0 ]; then
-#     echo "❌  데이터베이스에 연결할 수 없습니다. 종료합니다."
-# 	sleep 1000
-#     exit 1
-#   fi
-#   echo "⏳  DB 연결 대기 중..."
-#   MAX_ATTEMPTS=$((MAX_ATTEMPTS - 1))
-#   sleep 1
-# done
+echo "🚀 서버 시작"
 
-# echo "✅  DB 연결 성공! 마이그레이션 실행"
 
-# npx sequelize-cli db:migrate
-# # 필요 시 시드 실행
-# # npx sequelize-cli db:seed:all
-
-echo "🚀  서버 시작"
-exec npm run dev
+# 환경변수에 따라 실행 모드 결정
+if [ "$NODE_ENV" = "production" ]; then
+    npm install --production
+    echo "🏭 Production 모드로 실행"
+    exec npm run start
+else
+    npm install --development
+    echo "🛠️ Development 모드로 실행"
+    exec npm run dev
+fi
